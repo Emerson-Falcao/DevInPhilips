2d8ad2812bd5314d1cda4f8d0ec4465a3f1f3b75
Emerson-Falcao <urifalco@gmail.com>
2022-10-21 20:19:59 -0300

    [M1S03] Ex 6 - Palíndromo

---
+++ Modulo1/semana03/[M1S03] Ex 6/app.js
@@ -0,0 +1,7 @@
+const verificaPalindromo = (str) => str === str.split("").reverse().join("")
+
+const teste1 = verificaPalindromo("tenet")
+console.log(teste1)
+
+const teste2 = verificaPalindromo("julia")
+console.log(teste2)
