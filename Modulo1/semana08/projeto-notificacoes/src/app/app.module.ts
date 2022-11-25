f896a7af64b736ad0833fcc6df2352388ff93992
Emerson-Falcao <urifalco@gmail.com>
2022-11-25 20:08:18 -0300

    [M1S08] Ex 6 - Transforme o menu superior e o rodapé e um componente para cada (header e footer)

--- Modulo1/semana08/projeto-notificacoes/src/app/app.module.ts
+++ Modulo1/semana08/projeto-notificacoes/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotifHeaderComponent } from './notif-header/notif-header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotifHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ Modulo1/semana08/projeto-notificacoes/src/app/notif-header/notif-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ntap-notif-header',
+  templateUrl: './notif-header.component.html',
+  styleUrls: ['./notif-header.component.scss']
+})
+export class NotifHeaderComponent {
+
+}
