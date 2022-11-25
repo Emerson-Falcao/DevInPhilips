efa160344a779ff430e8b0d5a8146cf6f281872f
Emerson-Falcao <urifalco@gmail.com>
2022-11-25 20:21:36 -0300

    [M1S08] Ex 7 - Mover valores estáticos que estão no template para passar a olhar para o TS do componente.

--- Modulo1/semana08/projeto-notificacoes/src/app/app.component.ts
+++ Modulo1/semana08/projeto-notificacoes/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-notificacoes';
+  notificacoes = [
+    {
+      titulo:'Novo pedido de amizade!',
+      subtitulo:'Uma pessoa te mandou um pedido de amizade!',
+      data: new Date('2022-11-26')
+    },
+    {
+      titulo:'Curtida!',
+      subtitulo:'Sua postagem foi curtida',
+      data: new Date('2022-11-25')
+    },
+    {
+      titulo:'Novo comentário!',
+      subtitulo:'Sua postagem recebeu um novo comentário',
+      data: new Date('2022-11-25')
+    }
+  ]
 }
