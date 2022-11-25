a9e2ad0b6fb0030737abf44cb3e1f84716364e37
Emerson-Falcao <urifalco@gmail.com>
2022-11-25 20:44:16 -0300

    [M1S08] Ex 8 - Extrair da variável para uma arquivo typescript só para armazenar os valores estáticos

--- Modulo1/semana08/projeto-notificacoes/src/app/app.component.ts
+++ Modulo1/semana08/projeto-notificacoes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NOTIFICACOES } from './mocks/notificacoes-mock';
 
 @Component({
   selector: 'ntap-root',
@@ -7,21 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-notificacoes';
-  notificacoes = [
-    {
-      titulo:'Novo pedido de amizade!',
-      subtitulo:'Uma pessoa te mandou um pedido de amizade!',
-      data: new Date('2022-11-26')
-    },
-    {
-      titulo:'Curtida!',
-      subtitulo:'Sua postagem foi curtida',
-      data: new Date('2022-11-25')
-    },
-    {
-      titulo:'Novo comentário!',
-      subtitulo:'Sua postagem recebeu um novo comentário',
-      data: new Date('2022-11-25')
-    }
-  ]
+  notificacoes = NOTIFICACOES;
 }
---
+++ Modulo1/semana08/projeto-notificacoes/src/app/mocks/notificacoes-mock.ts
@@ -0,0 +1,17 @@
+export const NOTIFICACOES = [
+    {
+      titulo:'Novo pedido de amizade!',
+      subtitulo:'Uma pessoa te mandou um pedido de amizade!',
+      data: new Date('2022-11-26')
+    },
+    {
+      titulo:'Curtida!',
+      subtitulo:'Sua postagem foi curtida',
+      data: new Date('2022-11-25')
+    },
+    {
+      titulo:'Novo comentário!',
+      subtitulo:'Sua postagem recebeu um novo comentário',
+      data: new Date('2022-11-25')
+    }
+  ]
