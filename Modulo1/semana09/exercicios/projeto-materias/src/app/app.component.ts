acc09b8094b19d7b9ae44524e9248fbbb5575a27
Emerson-Falcao <urifalco@gmail.com>
2022-12-04 18:17:11 -0300

    [M1S09] Ex 6 - @Input (Parte 3)

--- Modulo1/semana09/exercicios/projeto-materias/src/app/app.component.ts
+++ Modulo1/semana09/exercicios/projeto-materias/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-materias';
+  personagem: any = {
+    nome: 'Aragorn Elessar',
+    imagem: '../../assets/aragorn.jpg',
+    nomeAtor: 'Viggo Mortensen',
+    descricao:
+      'Aragorn, filho de Arathorn, também conhecido como Passo-Longo; foi o líder dos Dunedain do norte de Arnor e após a derrota de Sauron, Rei Elessar dos reinos unificados de Gondor e Arnor.',
+  };
 }
--- Modulo1/semana09/exercicios/projeto-materias/src/app/item-card/item-card.component.ts
+++ Modulo1/semana09/exercicios/projeto-materias/src/app/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -7,12 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ItemCardComponent {
   
-  personagem: any = {
-    nome: 'Aragorn Elessar',
-    imagem: '../../assets/aragorn.jpg',
-    nomeAtor: 'Viggo Mortensen',
-    descricao:
-      'Aragorn, filho de Arathorn, também conhecido como Passo-Longo; foi o líder dos Dunedain do norte de Arnor e após a derrota de Sauron, Rei Elessar dos reinos unificados de Gondor e Arnor.',
-  };
+  @Input() personagem: any = {}
 
 }
