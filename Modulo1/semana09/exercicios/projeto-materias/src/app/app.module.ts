57113af99bca21bd9a55423c372e49023559d1d3
Emerson-Falcao <urifalco@gmail.com>
2022-12-04 17:47:04 -0300

    [M1S09] Ex 3 - Componentes

--- Modulo1/semana09/exercicios/projeto-materias/src/app/app.module.ts
+++ Modulo1/semana09/exercicios/projeto-materias/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ModalComponent } from './modal/modal.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ModalComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
