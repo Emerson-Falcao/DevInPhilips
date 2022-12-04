f34649b6c5a8f866fcda267fc189ae9d6551d023
Emerson-Falcao <urifalco@gmail.com>
2022-12-04 18:10:58 -0300

    [M1S09] Ex 5 - Property Binding (Parte 2)

--- Modulo1/semana09/exercicios/projeto-materias/src/app/item-card/item-card.component.ts
+++ Modulo1/semana09/exercicios/projeto-materias/src/app/item-card/item-card.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent {
+  
+  personagem: any = {
+    nome: 'Aragorn Elessar',
+    imagem: '../../assets/aragorn.jpg',
+    nomeAtor: 'Viggo Mortensen',
+    descricao:
+      'Aragorn, filho de Arathorn, também conhecido como Passo-Longo; foi o líder dos Dunedain do norte de Arnor e após a derrota de Sauron, Rei Elessar dos reinos unificados de Gondor e Arnor.',
+  };
 
 }
