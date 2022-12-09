b1a761ce0f802a809dfa402bc3c53f2152682a87
Emerson-Falcao <urifalco@gmail.com>
2022-12-09 18:26:40 -0300

    [M1S10] Ex 5 - Configuração de roteamento

--- Modulo1/semana08/projeto-notificacoes/src/app/pages/home/home.component.ts
+++ Modulo1/semana08/projeto-notificacoes/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NOTIFICACOES } from 'src/app/mocks/notificacoes-mock';
 
 @Component({
   selector: 'ntap-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  notificacoes = NOTIFICACOES;
+  notificacaoLida(notificacao:any){
+    notificacao.lido = true
+  }
 }
