59d38cb990a375992a585f134eabcc0d0d0ee898
Emerson-Falcao <urifalco@gmail.com>
2023-02-19 14:28:11 -0300

    [M2S02] Ex 2 - Acesse elementos de um Array (multidimensional)

--- Modulo2/src/semana1/MainSemana1.java
+++ Modulo2/src/semana1/MainSemana1.java
@@ -6,6 +6,9 @@ public class MainSemana1 {
     public static void main(String args[]){
         System.out.println("Exercicio 1:");
         Exercicio1.exercicio1();
+        System.out.println("-------------------------");
+        System.out.println("Exercicio 2:");
+        Exercicio2.exercicio2();
     }
 
 }
