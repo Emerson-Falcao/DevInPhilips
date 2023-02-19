fa6f60d5d79e9daf74de1455b3b7628f467a8607
Emerson-Falcao <urifalco@gmail.com>
2023-02-19 14:20:14 -0300

    [M2S02] Ex 1 - Acesse elementos de um Array

---
+++ Modulo2/src/semana1/Exercicio1.java
@@ -0,0 +1,14 @@
+package semana1;
+
+
+public class Exercicio1 {
+
+    static int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+    
+    public static void exercicio1(){
+        System.out.println("Primeiro valor: "+arr[0]);
+        System.out.println("Ultimo valor: "+arr[arr.length-1]);
+        System.out.println("Terceiro valor: "+arr[2]);
+    }
+
+}
--- Modulo2/src/semana1/MainSemana1.java
+++ Modulo2/src/semana1/MainSemana1.java
@@ -3,6 +3,9 @@ package semana1;
 
 public class MainSemana1 {
 
-
+    public static void main(String args[]){
+        System.out.println("Exercicio 1:");
+        Exercicio1.exercicio1();
+    }
 
 }
