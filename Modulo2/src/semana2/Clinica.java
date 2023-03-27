f9310583000ea2dc45fb64aad8cb91c085f38149
Emerson-Falcao <urifalco@gmail.com>
2023-03-27 19:23:30 -0300

    [M2S03] Ex 2 - Cálculo do IMC

--- Modulo2/src/semana2/Clinica.java
+++ Modulo2/src/semana2/Clinica.java
@@ -9,4 +9,25 @@ public class Clinica {
     String endereco;
     int codigoClinica;
 
+    
+    public double calculoIMC(double peso, double altura){
+        
+        double imc = peso / altura*altura;
+        
+        if(imc < 18.5)
+            System.out.println("Abaixo do peso normal");
+        else if(imc < 24.9)
+            System.out.println("Peso normal");
+        else if(imc <29.9)
+            System.out.println("Excesso de peso");
+        else if(imc <34.9)
+            System.out.println("Obesidade I");
+        else if(imc <39.9)
+            System.out.println("Obesidade II");
+        else 
+            System.out.println("Obesidade III");
+        
+        return imc;
+    }
+    
 }
